6f3df0df22c7cd163df2d7c69e0917774c7f4c51
Oakmoater <dubrovecva@gmail.com>
2024-02-04 18:30:06 +0100

    18-30

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/Nemezis_Wellspa">
           <StyleSheetManager shouldForwardProp={isPropValid}>
             <GlobalStyle />
             <ThemeProvider theme={theme}>
